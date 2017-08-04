b3d197135c82d9993923fb3562eafe936c3e5a95
Stephen Johnston <stephenj@launchpowered.com>
2017-08-04 07:04:10 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,13 @@ gulp.task('run-tests', ['lint', 'test-coverage']);
 
 gulp.task('build', ['build-bundle-prod']);
 
+gulp.task('buildpubwise', ['build-bundle-prod','move-pw-build']);
+
+gulp.task('move-pw-build', function () {
+  gulp.src("build/dist/prebid.js")
+      .pipe(gulp.dest('build/pwdeploy'));  
+}
+
 gulp.task('clean', function () {
   return gulp.src(['build'], {
       read: false
