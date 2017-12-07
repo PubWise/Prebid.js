a0ff8cef7f9904adae7f72913c6e75fc5e90af53
GLStephen <stephenj@launchpowered.com>
2017-12-07 13:35:58 +0000

    Updates for Session Handling

--- modules/pubwiseAnalyticsAdapter.js
+++ modules/pubwiseAnalyticsAdapter.js
@@ -23,19 +23,47 @@ const utils = require('src/utils');
 const analyticsType = 'endpoint';
 const analyticsName = 'PubWise Analytics: ';
 let defaultUrl = 'https://api.pubwise.io/api/v4/event/default/';
-let pubwiseVersion = '2.2';
-let configOptions = {site: '', endpoint: 'https://api.pubwise.io/api/v4/event/default/', debug: ''};
+let pwVersion = '2.3.0';
+let configOptions = {site: '', endpoint: 'https://api.pubwise.io/api/v4/event/default/', ignoreAdUnitCodes: [], debug: ''};
 let pwAnalyticsEnabled = false;
 let utmKeys = {utm_source: '', utm_medium: '', utm_campaign: '', utm_term: '', utm_content: ''};
+let sessionData = {sessionId: ''};
+let pwNamespace = 'pubwise';
+let targetSet = 0;
+let initSent = false;
+let sessTimeout = 60 * 30 * 1000; // 30 minutes, G Analytics default session length
+let sessName = 'sess_id';
+let sessTimeoutName = 'sess_timeout';
 
 function markEnabled() {
   utils.logInfo(`${analyticsName}Enabled`, configOptions);
   pwAnalyticsEnabled = true;
 }
 
+function enrichWithSessionInfo(dataBag) {
+  try {
+    dataBag['session_id'] = sessionData.sessId;
+  } catch (e) {
+    dataBag['error_sess'] = 1;
+  }
+
+  return dataBag;
+}
+
+function enrichWithEnvironmentInfo(dataBag) {
+  try {
+    dataBag['target_site'] = configOptions.site;
+    dataBag['debug'] = configOptions.debug ? 1 : 0;
+    dataBag['pw_version'] = pwVersion;
+  } catch (e) {
+    dataBag['error_env'] = 1;
+  }
+
+  return dataBag;
+}
+
 function enrichWithMetrics(dataBag) {
   try {
-    dataBag['pw_version'] = pubwiseVersion;
     dataBag['pbjs_version'] = $$PREBID_GLOBAL$$.version;
     dataBag['debug'] = configOptions.debug;
   } catch (e) {
@@ -49,8 +77,7 @@ function enrichWithUTM(dataBag) {
   let newUtm = false;
   try {
     for (let prop in utmKeys) {
-      let urlValue = utils.getParameterByName(prop);
-      utmKeys[prop] = urlValue;
+      utmKeys[prop] = utils.getParameterByName(prop);
       if (utmKeys[prop] != '') {
         newUtm = true;
         dataBag[prop] = utmKeys[prop];
@@ -59,14 +86,14 @@ function enrichWithUTM(dataBag) {
 
     if (newUtm === false) {
       for (let prop in utmKeys) {
-        let itemValue = localStorage.getItem(`pw-${prop}`);
-        if (itemValue.length !== 0) {
+        let itemValue = localStorage.getItem(setNamespace(prop));
+        if (itemValue !== null && typeof itemValue !== 'undefined' && itemValue.length !== 0) {
           dataBag[prop] = itemValue;
         }
       }
     } else {
       for (let prop in utmKeys) {
-        localStorage.setItem(`pw-${prop}`, utmKeys[prop]);
+        localStorage.setItem(setNamespace(prop), utmKeys[prop]);
       }
     }
   } catch (e) {
@@ -76,24 +103,75 @@ function enrichWithUTM(dataBag) {
   return dataBag;
 }
 
-function sendEvent(eventType, data) {
-  utils.logInfo(`${analyticsName}Event ${eventType} ${pwAnalyticsEnabled}`, data);
+function setNamespace(itemText) {
+  return pwNamespace.concat('_' + itemText);
+}
 
-  // put the typical items in the data bag
-  let dataBag = {
-    eventType: eventType,
-    args: data,
-    target_site: configOptions.site,
-    debug: configOptions.debug ? 1 : 0,
-  };
+function localStorageSessTimeoutName() {
+  return setNamespace(sessTimeoutName);
+}
 
-  // for certain events, track additional info
-  if (eventType == CONSTANTS.EVENTS.AUCTION_INIT) {
-    dataBag = enrichWithMetrics(dataBag);
-    dataBag = enrichWithUTM(dataBag);
+function localStorageSessName() {
+  return setNamespace(sessName);
+}
+
+function extendUserSessionTimeout() {
+  localStorage.setItem(localStorageSessTimeoutName(), Date.now());
+}
+
+function userSessionID() {
+  return localStorage.getItem(localStorageSessName()) ? localStorage.getItem(localStorageSessName()) : '';
+}
+
+function sessionExpired() {
+  let sessLastTime = localStorage.getItem(localStorageSessTimeoutName());
+  return (Date.now() - sessLastTime) > sessTimeout;
+}
+
+function isValidEvent(eventType, data) {
+  let validEvent = true;
+  if (typeof data !== 'undefined' && typeof data.args !== 'undefined' && (typeof data.args.auctionId !== 'undefined' || typeof data.args.requestId !== 'undefined')) {
+    if (eventType == CONSTANTS.EVENTS.BID_REQUESTED && (typeof data.args.bids === 'undefined' || data.args.bids.length === 0)) {
+      validEvent = false;
+    }
+  } else {
+    validEvent = false;
   }
 
-  ajax(configOptions.endpoint, (result) => utils.logInfo(`${analyticsName}Result`, result), JSON.stringify(dataBag));
+  return validEvent;
+}
+
+function isUnitIgnored(eventType, data) {
+  let ignoreStatus = false;
+  if (typeof data !== 'undefined' && typeof data.args !== 'undefined') {
+    if (eventType == CONSTANTS.EVENTS.BID_RESPONSE ||
+      eventType == CONSTANTS.EVENTS.BID_ADJUSTMENT ||
+      eventType == CONSTANTS.EVENTS.BID_WON) {
+      if (typeof data.args.adUnitCode !== 'undefined' && configOptions.ignoreAdUnitCodes.includes(data.args.adUnitCode)) {
+        // no data change needed
+        ignoreStatus = true;
+      }
+    }
+  }
+  return ignoreStatus;
+}
+
+function removeIgnoredBids(eventType, data) {
+  if (typeof data !== 'undefined' && typeof data.args !== 'undefined') {
+    if (eventType == CONSTANTS.EVENTS.BID_REQUESTED) {
+      if (typeof data.args.bids !== 'undefined') {
+        for (var i = data.args.bids.length; i--;) {
+          if (configOptions.ignoreAdUnitCodes.includes(data.args.bids[i].adUnitCode)) {
+            // remove the bid requests that are ignored, valid checks will handle creation of invalid data later
+            data.args.bids.splice(i, 1);
+            utils.logInfo(`${analyticsName}Event ${eventType} Ignore Removed`, data);
+          }
+        }
+      }
+    }
+  }
+
+  return data;
 }
 
 let pubwiseAnalytics = Object.assign(adapter(
@@ -104,17 +182,121 @@ let pubwiseAnalytics = Object.assign(adapter(
 {
   // Override AnalyticsAdapter functions by supplying custom methods
   track({eventType, args}) {
-    sendEvent(eventType, args);
+    utils.logInfo(`${analyticsName} ${eventType} track`);
+    this.handleEvent(eventType, args);
   }
 });
 
-pubwiseAnalytics.adapterEnableAnalytics = pubwiseAnalytics.enableAnalytics;
+pubwiseAnalytics.handleEvent = function(eventType, data) {
+  utils.logInfo(`${analyticsName}Event ${eventType} ${pwAnalyticsEnabled}`, data);
 
-pubwiseAnalytics.enableAnalytics = function (config) {
+  // put the typical items in the data bag
+  let dataBag = {eventType: eventType, args: data};
+
+  dataBag = enrichWithEnvironmentInfo(dataBag);
+
+  // handle bid response settings
+  if (eventType == CONSTANTS.EVENTS.SET_TARGETING) {
+    targetSet = 1;
+  }
+
+  // handle bid response settings
+  if (eventType == CONSTANTS.EVENTS.BID_RESPONSE) {
+    dataBag['targetSet'] = targetSet;
+  }
+
+  if (eventType == CONSTANTS.EVENTS.AUCTION_END) {
+    this.ensureSession();
+  }
+
+  // for certain events, track additional info
+  if (eventType == CONSTANTS.EVENTS.AUCTION_INIT) {
+    this.ensureSession();
+    dataBag = enrichWithSessionInfo(dataBag);
+    dataBag = enrichWithMetrics(dataBag);
+    dataBag = enrichWithUTM(dataBag);
+
+    // look at the initialization state
+    if (initSent === false) {
+      if (typeof PREBID_TIMEOUT !== 'undefined') {
+        dataBag['targetTimeout'] = PREBID_TIMEOUT;
+      } else {
+        dataBag['targetTimeout'] = 'NA';
+      }
+    } else {
+      dataBag['targetTimeout'] = '';
+    }
+    initSent = true;
+  }
+
+  // first we remove bids, then test for validity then continuing
+  dataBag = removeIgnoredBids(eventType, dataBag);
+
+  if (isValidEvent(eventType, dataBag) === true) {
+    if (isUnitIgnored(eventType, dataBag) === false) {
+      if (eventType != CONSTANTS.EVENTS.ADD_AD_UNITS && eventType != CONSTANTS.EVENTS.SET_TARGETING && eventType != CONSTANTS.EVENTS.REQUEST_BIDS && eventType != CONSTANTS.EVENTS.BID_ADJUSTMENT && eventType != CONSTANTS.EVENTS.AUCTION_END) {
+        this.sendEventToEndpoint(configOptions.endpoint, dataBag);
+      } else {
+        this.filterAnalyticsEvent(eventType, dataBag);
+      }
+    } else {
+      this.ignoreAnalyticsEvent(eventType, dataBag);
+    }
+  } else {
+    this.invalidAnalyticsEvent(eventType, dataBag);
+  }
+};
+
+pubwiseAnalytics.sendEventToEndpoint = function(endpoint, dataBag) {
+  ajax(endpoint, (result) => utils.logInfo(`${analyticsName}Event Status is Sent`, result), JSON.stringify(dataBag));
+};
+
+pubwiseAnalytics.invalidAnalyticsEvent = function (eventType, dataBag) {
+  this.logEventDisposition('Invalid', eventType, dataBag);
+};
+
+pubwiseAnalytics.filterAnalyticsEvent = function (eventType, dataBag) {
+  this.logEventDisposition('Filtered', eventType, dataBag);
+};
+
+pubwiseAnalytics.ignoreAnalyticsEvent = function (eventType, dataBag) {
+  this.logEventDisposition('Ignored', eventType, dataBag);
+};
+
+pubwiseAnalytics.logEventDisposition = function (disposition, eventType, dataBag) {
+  utils.logInfo(`${analyticsName}Event Status is ${disposition} for ${eventType}`, dataBag);
+};
+
+pubwiseAnalytics.setConfig = function (config) {
   if (config.options.debug === undefined) {
     config.options.debug = utils.debugTurnedOn();
   }
   configOptions = config.options;
+
+  if (typeof configOptions.ignoreAdUnitCodes === 'undefined') {
+    configOptions.ignoreAdUnitCodes = [];
+  }
+};
+
+pubwiseAnalytics.storeSessionID = function (userSessID) {
+  localStorage.setItem(localStorageSessName(), userSessID);
+};
+
+// ensure a session exists, if not make one, always store it
+pubwiseAnalytics.ensureSession = function () {
+  // namespace.concat();
+  if (sessionExpired() === true) {
+    let userSessID = utils.generateUUID();
+    this.storeSessionID(userSessID);
+  }
+  sessionData.sessId = userSessionID();
+  extendUserSessionTimeout();
+};
+
+pubwiseAnalytics.adapterEnableAnalytics = pubwiseAnalytics.enableAnalytics;
+
+pubwiseAnalytics.enableAnalytics = function (config) {
+  this.setConfig(config);
   markEnabled();
   pubwiseAnalytics.adapterEnableAnalytics(config);
 };
--- test/spec/modules/pubwiseAnalyticsAdapter_spec.js
+++ test/spec/modules/pubwiseAnalyticsAdapter_spec.js
@@ -1,35 +1,470 @@
 import pubwiseAnalytics from 'modules/pubwiseAnalyticsAdapter';
+import pubwiseAnalytics0 from 'modules/pubwiseAnalyticsAdapter';
+import pubwiseAnalytics1 from 'modules/pubwiseAnalyticsAdapter';
+import pubwiseAnalytics2 from 'modules/pubwiseAnalyticsAdapter';
+import {expect} from 'chai';
+
 let events = require('src/events');
 let adaptermanager = require('src/adaptermanager');
+let adaptermanager1 = require('src/adaptermanager');
 let constants = require('src/constants.json');
 
-describe('PubWise Prebid Analytics', function () {
-  describe('enableAnalytics', function () {
-    it('should catch all events', function () {
-      sinon.spy(pubwiseAnalytics, 'track');
+let sendableEvents = [
+  {
+    name: constants.EVENTS.AUCTION_INIT,
+    valid: {timestamp: 1000000000000, auctionId: 'auctiona-test-test-test-testtesttest', timeout: 1000}
+  },
+  {
+    name: constants.EVENTS.BID_REQUESTED,
+    valid: {
+      auctionId: 'auctiona-test-test-test-testtesttest',
+      auctionStart: 1000000000000,
+      bidderCode: 'testc',
+      bidderRequestId: '13d0a64a59babe',
+      bids: [{
+        adUnitCode: 'test-test-1000',
+        auctionId: 'auctiona-test-test-test-testtesttest',
+        bidId: 'thisisabidid',
+        bidder: 'testc',
+        bidderRequestId: 'thisisabidid',
+        params: {siteId: '0000', adSizes: '728x90'},
+        sizes: [[728, 90]],
+        transactionId: 'transact-test-test-test-testtesttest'
+      }, {
+        adUnitCode: 'test-test-2000',
+        auctionId: 'auctiona-test-test-test-testtesttest',
+        bidId: 'thisisabidid',
+        bidder: 'testc',
+        bidderRequestId: 'thisisabidid',
+        params: {siteId: '0000', adSizes: '728x90'},
+        sizes: [[728, 90]],
+        transactionId: 'transact-test-test-test-testtesttest'
+      }],
+      doneCbCallCount: 1,
+      start: 1000000000000,
+      timeout: 1000
+    }
+  },
+  {
+    name: constants.EVENTS.BID_RESPONSE,
+    valid: {
+      ad: '<div>test ad only</div>',
+      adId: '45ccb8c7da9a63',
+      adUnitCode: 'test-test-1000',
+      adserverTargeting: {hb_bidder: 'jcm', hb_adid: '45ccb8c7da9a63', hb_pb: '1.58', hb_size: '728x90'},
+      auctionId: 'testaaaa-test-test-test-testtesttest',
+      bidder: 'jcm',
+      bidderCode: 'jcm',
+      cpm: 1.5875999,
+      creativeId: 'jcm45ccb8c7da9a63',
+      currency: 'USD',
+      height: 90,
+      mediaType: 'banner',
+      netRevenue: true,
+      pbAg: '1.55',
+      pbCg: '',
+      pbDg: '1.58',
+      pbHg: '1.58',
+      pbLg: '1.50',
+      pbMg: '1.50',
+      requestTimestamp: 1511901262899,
+      responseTimestamp: 1511901262985,
+      size: '728x90',
+      statusMessage: 'Bid available',
+      timeToRespond: 86,
+      ttl: 60,
+      width: 728
+    }
+  },
+  {
+    name: constants.EVENTS.BID_WON,
+    valid: {
+      auctionId: 'testaaaa-test-test-test-testtesttest',
+      adUnitCode: 'test-test-1000'
+    }
+  },
+  {
+    name: constants.EVENTS.BID_TIMEOUT,
+    valid: {
+      auctionId: 'testaaaa-test-test-test-testtesttest'
+    }
+  }
+];
 
-      adaptermanager.registerAnalyticsAdapter({
-        code: 'pubwiseanalytics',
-        adapter: pubwiseAnalytics
+let nonSendableEvents = [
+  {
+    name: constants.EVENTS.SET_TARGETING,
+    valid: {
+      auctionId: 'testaaaa-test-test-test-testtesttest'
+    }
+  },
+  {
+    name: constants.EVENTS.AUCTION_END,
+    valid: {
+      auctionId: 'testaaaa-test-test-test-testtesttest'
+    }
+  }
+];
+
+let ignoreTestEvents = [{
+  name: constants.EVENTS.BID_REQUESTED,
+  valid: {
+    auctionId: 'auctiona-test-test-test-testtesttest',
+    auctionStart: 1000000000000,
+    bidderCode: 'testc',
+    bidderRequestId: '13d0a64a59babe',
+    bids: [{
+      adUnitCode: 'test-test-1000',
+      auctionId: 'auctiona-test-test-test-testtesttest',
+      bidId: 'thisisabidid',
+      bidder: 'testc',
+      bidderRequestId: 'thisisabidid',
+      params: {siteId: '0000', adSizes: '728x90'},
+      sizes: [[728, 90]],
+      transactionId: 'transact-test-test-test-testtesttest'
+    }, {
+      adUnitCode: 'test-test-2000',
+      auctionId: 'auctiona-test-test-test-testtesttest',
+      bidId: 'thisisabidid',
+      bidder: 'testc',
+      bidderRequestId: 'thisisabidid',
+      params: {siteId: '0000', adSizes: '728x90'},
+      sizes: [[728, 90]],
+      transactionId: 'transact-test-test-test-testtesttest'
+    }],
+    doneCbCallCount: 1,
+    start: 1000000000000,
+    timeout: 1000
+  }
+}, {
+  name: constants.EVENTS.BID_RESPONSE,
+  valid: {
+    ad: '<div>test ad only</div>',
+    adId: '45ccb8c7da9a63',
+    adUnitCode: 'test-test-1000',
+    adserverTargeting: {hb_bidder: 'jcm', hb_adid: '45ccb8c7da9a63', hb_pb: '1.58', hb_size: '728x90'},
+    auctionId: 'testaaaa-test-test-test-testtesttest',
+    bidder: 'jcm',
+    bidderCode: 'jcm',
+    cpm: 1.5875999,
+    creativeId: 'jcm45ccb8c7da9a63',
+    currency: 'USD',
+    height: 90,
+    mediaType: 'banner',
+    netRevenue: true,
+    pbAg: '1.55',
+    pbCg: '',
+    pbDg: '1.58',
+    pbHg: '1.58',
+    pbLg: '1.50',
+    pbMg: '1.50',
+    requestTimestamp: 1511901262899,
+    responseTimestamp: 1511901262985,
+    size: '728x90',
+    statusMessage: 'Bid available',
+    timeToRespond: 86,
+    ttl: 60,
+    width: 728
+  }
+}, {
+  name: constants.EVENTS.BID_WON,
+  valid: {
+    auctionId: 'testaaaa-test-test-test-testtesttest',
+    adUnitCode: 'test-test-1000'
+  }
+}, {
+  name: constants.EVENTS.BID_REQUESTED,
+  valid: {
+    auctionId: 'auctiona-test-test-test-testtesttest',
+    auctionStart: 1000000000000,
+    bidderCode: 'testc',
+    bidderRequestId: '13d0a64a59babe',
+    bids: [{
+      adUnitCode: 'test-test-noignore',
+      auctionId: 'auctiona-test-test-test-testtesttest',
+      bidId: 'thisisabidid',
+      bidder: 'testc',
+      bidderRequestId: 'thisisabidid',
+      params: {siteId: '0000', adSizes: '728x90'},
+      sizes: [[728, 90]],
+      transactionId: 'transact-test-test-test-testtesttest'
+    }, {
+      adUnitCode: 'test-test-2000',
+      auctionId: 'auctiona-test-test-test-testtesttest',
+      bidId: 'thisisabidid',
+      bidder: 'testc',
+      bidderRequestId: 'thisisabidid',
+      params: {siteId: '0000', adSizes: '728x90'},
+      sizes: [[728, 90]],
+      transactionId: 'transact-test-test-test-testtesttest'
+    }],
+    doneCbCallCount: 1,
+    start: 1000000000000,
+    timeout: 1000
+  }
+}, {
+  name: constants.EVENTS.BID_RESPONSE,
+  valid: {
+    ad: '<div>test ad only</div>',
+    adId: '45ccb8c7da9a63',
+    adUnitCode: 'test-test-noignore',
+    adserverTargeting: {hb_bidder: 'jcm', hb_adid: '45ccb8c7da9a63', hb_pb: '1.58', hb_size: '728x90'},
+    auctionId: 'testaaaa-test-test-test-testtesttest',
+    bidder: 'jcm',
+    bidderCode: 'jcm',
+    cpm: 1.5875999,
+    creativeId: 'jcm45ccb8c7da9a63',
+    currency: 'USD',
+    height: 90,
+    mediaType: 'banner',
+    netRevenue: true,
+    pbAg: '1.55',
+    pbCg: '',
+    pbDg: '1.58',
+    pbHg: '1.58',
+    pbLg: '1.50',
+    pbMg: '1.50',
+    requestTimestamp: 1511901262899,
+    responseTimestamp: 1511901262985,
+    size: '728x90',
+    statusMessage: 'Bid available',
+    timeToRespond: 86,
+    ttl: 60,
+    width: 728
+  }
+}, {
+  name: constants.EVENTS.BID_WON,
+  valid: {
+    auctionId: 'testaaaa-test-test-test-testtesttest',
+    adUnitCode: 'test-test-noignore'
+  }
+}, {
+  name: constants.EVENTS.BID_REQUESTED,
+  valid: {
+    auctionId: 'auctiona-test-test-test-testtesttest',
+    auctionStart: 1000000000000,
+    bidderCode: 'testc',
+    bidderRequestId: '13d0a64a59babe',
+    bids: [{
+      adUnitCode: 'test-test-1000',
+      auctionId: 'auctiona-test-test-test-testtesttest',
+      bidId: 'thisisabidid',
+      bidder: 'testc',
+      bidderRequestId: 'thisisabidid',
+      params: {siteId: '0000', adSizes: '728x90'},
+      sizes: [[728, 90]],
+      transactionId: 'transact-test-test-test-testtesttest'
+    }],
+    doneCbCallCount: 1,
+    start: 1000000000000,
+    timeout: 1000
+  }
+}];
+
+describe('Testing PubWise', function () {
+  adaptermanager.registerAnalyticsAdapter({
+    code: 'pubwise',
+    adapter: pubwiseAnalytics
+  });
+
+  adaptermanager.enableAnalytics([{
+    provider: 'pubwise',
+    options: {
+      site: ['test-test-test-test0']
+    }
+  }]);
+
+  describe('Sessions Are Managed', function () {
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'storeSessionID');
+    });
+
+    afterEach(() => {
+      pubwiseAnalytics.storeSessionID.restore();
+    });
+
+    describe('If no Session Exists then One Is Created', function () {
+      it(`Calls StoreSessionID to add`, function () {
+        localStorage.removeItem('pubwise_sess_id');
+        localStorage.removeItem('pubwise_sess_timeout');
+        events.emit(constants.EVENTS.AUCTION_INIT, {});
+        sinon.assert.callCount(pubwiseAnalytics.storeSessionID, 1);
+      });
+    });
+
+    describe('If Session Expires then a New One Is Created', function () {
+      it(`Calls storeSessionID to Add`, function () {
+        let defaultTimer = 1800000;
+        localStorage.setItem('pubwise_sess_timeout', Date.now() - 1800000 - 10); // 10 extra seconds to expire it
+        events.emit(constants.EVENTS.AUCTION_INIT, {});
+        sinon.assert.callCount(pubwiseAnalytics.storeSessionID, 1);
+      });
+    });
+
+    describe('ensureSession is only called for certain events', function () {
+      it(`ensureSession is Called on AUCTION_INIT and AUCTION_END`, function () {
+        sinon.spy(pubwiseAnalytics, 'ensureSession');
+        events.emit(constants.EVENTS.AUCTION_INIT, {});
+        events.emit(constants.EVENTS.AUCTION_END, {});
+        sinon.assert.callCount(pubwiseAnalytics.ensureSession, 2);
+        pubwiseAnalytics.ensureSession.restore();
       });
 
-      adaptermanager.enableAnalytics({
-        provider: 'pubwiseanalytics',
+      it(`but ensureSessions is not called for other events`, function () {
+        sinon.spy(pubwiseAnalytics, 'ensureSession');
+        events.emit(constants.EVENTS.BID_REQUESTED, {});
+        events.emit(constants.EVENTS.BID_RESPONSE, {});
+        events.emit(constants.EVENTS.BID_WON, {});
+        sinon.assert.callCount(pubwiseAnalytics.ensureSession, 0);
+        pubwiseAnalytics.ensureSession.restore();
+      });
+    });
+  });
+
+  describe('track events', function () {
+    let spyTrackAnalytics;
+
+    beforeEach(() => {
+      spyTrackAnalytics = sinon.spy(pubwiseAnalytics, 'track');
+    });
+
+    afterEach(() => {
+      spyTrackAnalytics.restore();
+    });
+
+    sendableEvents.forEach(function(element) {
+      it('should track ' + element.name, function () {
+        events.emit(element.name, {});
+        expect(spyTrackAnalytics.callCount).to.equal(1);
+      });
+    });
+
+    nonSendableEvents.forEach(function(element) {
+      it('should track ' + element.name, function () {
+        events.emit(element.name, {});
+        expect(spyTrackAnalytics.callCount).to.equal(1);
+      });
+    });
+  });
+
+  describe('Invalid events should be sent to invalidAnlyticsEvent', function () {
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'invalidAnalyticsEvent');
+    });
+
+    afterEach(() => {
+      pubwiseAnalytics.invalidAnalyticsEvent.restore();
+    });
+
+    sendableEvents.forEach(function(element) {
+      it(`${element.name} is invalid`, function () {
+        events.emit(element.name, {});
+        sinon.assert.callCount(pubwiseAnalytics.invalidAnalyticsEvent, 1);
+      });
+    });
+
+    nonSendableEvents.forEach(function(element) {
+      it(`${element.name} is invalid`, function () {
+        events.emit(element.name, {});
+        sinon.assert.callCount(pubwiseAnalytics.invalidAnalyticsEvent, 1);
+      });
+    });
+  });
+
+  describe('succeed events', function () {
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'sendEventToEndpoint');
+    });
+
+    afterEach(() => {
+      pubwiseAnalytics.sendEventToEndpoint.restore();
+    });
+
+    sendableEvents.forEach(function(element) {
+      it(`valid ${element.name} should be sent`, function () {
+        events.emit(element.name, element.valid);
+        sinon.assert.callCount(pubwiseAnalytics.sendEventToEndpoint, 1);
+      });
+    });
+  });
+
+  describe('Some Valid Events are Filtered Events', function () {
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'filterAnalyticsEvent');
+    });
+
+    afterEach(() => {
+      pubwiseAnalytics.filterAnalyticsEvent.restore();
+    });
+
+    nonSendableEvents.forEach(function(element) {
+      it(`${element.name} should be filtered`, function () {
+        events.emit(element.name, element.valid);
+        sinon.assert.callCount(pubwiseAnalytics.filterAnalyticsEvent, 1);
+      });
+    });
+  });
+
+  describe('When Ad Units are Ignored Init Always Goes', function () {
+    before(() => {
+      pubwiseAnalytics.setConfig({
+        provider: 'pubwise',
         options: {
-          site: ['test-test-test-test']
+          site: ['test-test-test-test4'],
+          ignoreAdUnitCodes: ['test-test-1000']
         }
       });
+    });
 
-      events.emit(constants.EVENTS.AUCTION_INIT, {});
-      events.emit(constants.EVENTS.BID_REQUESTED, {});
-      events.emit(constants.EVENTS.BID_RESPONSE, {});
-      events.emit(constants.EVENTS.BID_WON, {});
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'sendEventToEndpoint');
+    });
 
-      events.emit(constants.EVENTS.AUCTION_END, {});
-      events.emit(constants.EVENTS.BID_TIMEOUT, {});
+    afterEach(() => {
+      pubwiseAnalytics.sendEventToEndpoint.restore();
+    });
 
-      /* testing for 6 calls, including the 2 we're not currently tracking */
-      sinon.assert.callCount(pubwiseAnalytics.track, 6);
+    it(`Init should always go`, function () {
+      events.emit(sendableEvents[0].name, sendableEvents[0].valid);
+      sinon.assert.callCount(pubwiseAnalytics.sendEventToEndpoint, 1);
+    });
+  });
+
+  describe('When Events are for Ignored AdUnitCodes the event is Ignored', function () {
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'ignoreAnalyticsEvent');
+    });
+
+    afterEach(() => {
+      pubwiseAnalytics.ignoreAnalyticsEvent.restore();
+    });
+
+    // bidResponse data is for test-test-1000 and should be filtered
+    it(`${sendableEvents[2].name} should be ignored`, function () {
+      events.emit(sendableEvents[2].name, sendableEvents[2].valid);
+      sinon.assert.callCount(pubwiseAnalytics.ignoreAnalyticsEvent, 1);
+    });
+
+    // bidResponse data is for test-test-1000 and should be filtered
+    it(`${sendableEvents[3].name} should be ignored`, function () {
+      events.emit(sendableEvents[3].name, sendableEvents[3].valid);
+      sinon.assert.callCount(pubwiseAnalytics.ignoreAnalyticsEvent, 1);
+    });
+  });
+
+  describe('When BidRequested Events are for ONLY Ignored AdUnitCodes the event is DEEMED INVALID', function () {
+    beforeEach(() => {
+      sinon.spy(pubwiseAnalytics, 'invalidAnalyticsEvent');
+    });
+
+    afterEach(() => {
+      pubwiseAnalytics.invalidAnalyticsEvent.restore();
+    });
+
+    // ignoreTestEvents 6 - bidRequested for only test-test-1000
+    it(`${ignoreTestEvents[6].name} should be ignored`, function () {
+      events.emit(ignoreTestEvents[6].name, ignoreTestEvents[6].valid);
+      sinon.assert.callCount(pubwiseAnalytics.invalidAnalyticsEvent, 1);
     });
   });
 });
