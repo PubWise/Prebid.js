3e0c86a3d765f4417f1893d563c62b10ad09bb5b
GLStephen <stephenj@launchpowered.com>
2018-06-27 23:06:50 +0000

    setup for Prebid Build 01.15.00

--- modules/pubwiseAnalyticsAdapter.js
+++ modules/pubwiseAnalyticsAdapter.js
@@ -23,9 +23,9 @@ const utils = require('src/utils');
 const analyticsType = 'endpoint';
 const analyticsName = 'PubWise Analytics: ';
 let defaultUrl = 'https://api.pubwise.io/api/v4/event/default/';
-let pubwiseVersion = '3.0';
+let pubwiseVersion = '3.1';
 let pubwiseSchema = 'AVOCET';
-let configOptions = {site: '', endpoint: 'https://api.pubwise.io/api/v4/event/default/', debug: ''};
+let configOptions = {site: '', endpoint: 'https://api.pubwise.io/api/v4/event/default/', debug: '', c_host: '', c_script_type: '', c_slot1: '', c_slot2: '', c_slot3: '', c_slot4: ''};
 let pwAnalyticsEnabled = false;
 let utmKeys = {utm_source: '', utm_medium: '', utm_campaign: '', utm_term: '', utm_content: ''};
 
@@ -60,7 +60,7 @@ function enrichWithUTM(dataBag) {
     if (newUtm === false) {
       for (let prop in utmKeys) {
         let itemValue = localStorage.getItem(`pw-${prop}`);
-        if (itemValue.length !== 0) {
+        if (itemValue !== null && itemValue.length !== 0) {
           dataBag[prop] = itemValue;
         }
       }
@@ -73,6 +73,35 @@ function enrichWithUTM(dataBag) {
     utils.logInfo(`${analyticsName}Error`, e);
     dataBag['error_utm'] = 1;
   }
+  return dataBag;
+}
+
+function enrichWithCustomSegments(dataBag) {
+  // c_site: '', c_script_type: '', c_slot1: '', c_slot2: '', c_slot3: '', c_slot4: ''
+  if (configOptions.c_host != '') {
+    dataBag['c_host'] = configOptions.c_host;
+  }
+
+  if (configOptions.c_script_type != '') {
+    dataBag['c_script_type'] = configOptions.c_script_type;
+  }
+
+  if (configOptions.c_slot1 != '') {
+    dataBag['c_slot1'] = configOptions.c_slot1;
+  }
+
+  if (configOptions.c_slot2 != '') {
+    dataBag['c_slot2'] = configOptions.c_slot2;
+  }
+
+  if (configOptions.c_slot3 != '') {
+    dataBag['c_slot3'] = configOptions.c_slot3;
+  }
+
+  if (configOptions.c_slot4 != '') {
+    dataBag['c_slot4'] = configOptions.c_slot4;
+  }
+
   return dataBag;
 }
 
@@ -92,6 +121,7 @@ function sendEvent(eventType, data) {
   // for certain events, track additional info
   if (eventType == CONSTANTS.EVENTS.AUCTION_INIT) {
     dataBag = enrichWithUTM(dataBag);
+    dataBag = enrichWithCustomSegments(dataBag);
   }
 
   ajax(configOptions.endpoint, (result) => utils.logInfo(`${analyticsName}Result`, result), JSON.stringify(dataBag));
