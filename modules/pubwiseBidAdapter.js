a672216868a056560e2727685b7941282a139f1f
Stephen Johnston <stephenj@launchpowered.com>
2023-02-02 19:29:07 +0000

    version update for bidder

--- modules/pubwiseBidAdapter.js
+++ modules/pubwiseBidAdapter.js
@@ -6,7 +6,7 @@ import { config } from '../src/config.js';
 import { convertOrtbRequestToProprietaryNative } from '../src/native.js';
 import { OUTSTREAM, INSTREAM } from '../src/video.js';
 
-const VERSION = '0.2.1';
+const VERSION = '0.2.2';
 const GVLID = 842;
 const NET_REVENUE = true;
 const UNDEFINED = undefined;
